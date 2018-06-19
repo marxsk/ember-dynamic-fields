802d34f5ed0248c892f6898a0649e939d8275175
Marek 'marx' Grác <mgrac@redhat.com>
2018-06-19 16:41:57 +0200

    [feat] State of the component holds also empty record

--- addon/components/form-dynamic.js
+++ addon/components/form-dynamic.js
@@ -1,6 +1,36 @@
-import Component from '@ember/component';
-import layout from '../templates/components/form-dynamic';
+import Ember from "ember";
+import Component from "@ember/component";
+import layout from "../templates/components/form-dynamic";
 
 export default Component.extend({
-  layout
+  layout,
+  _source: undefined,
+
+  isEmpty(record) {
+    return record === "";
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    if (Ember.isArray(this.get("dataObject"))) {
+      this.set("_source", this.get("dataObject"));
+    } else if (typeof this.get("dataObject") !== "undefined") {
+      Ember.Logger.assert(
+        "dataObject have to be Ember.Array if you want to pass data in"
+      );
+      return;
+    } else {
+      Ember.Logger.assert("No object was entered - unable to create one");
+      return;
+    }
+
+    if (!this.isEmpty(this.get("_source.lastObject"))) {
+      if (typeof this.get("_source.firstObject") === "string") {
+        this.get("_source").pushObject("");
+      } else {
+        this.get("_source").pushObject(Ember.Object.create());
+      }
+    }
+  }
 });
--- tests/integration/components/form-dynamic-test.js
+++ tests/integration/components/form-dynamic-test.js
@@ -8,19 +8,22 @@ import Ember from "ember";
 module("Integration | Component | form-dynamic", function(hooks) {
   setupRenderingTest(hooks);
 
-  test("it yields content once when no data are available", async function(assert) {
-    await render(hbs`{{form-dynamic}}`);
+  test("it yields content once when only empty dataset available", async function(assert) {
+    this.set("data", Ember.A());
+    await render(hbs`{{form-dynamic dataObject=data}}`);
     assert.equal(this.element.textContent.trim(), "");
 
+    this.set("data2", Ember.A());
+    // @todo: use 'data' instead of data2 - content is preserved, why??
     await render(hbs`
-      {{#form-dynamic}}
+      {{#form-dynamic dataObject=data2}}
         template block text
       {{/form-dynamic}}
     `);
     assert.equal(this.element.textContent.trim(), "template block text");
   });
 
-  test("it yields content according to number of records in dataObject", async function(assert) {
+  test("it yields fields according to number of records in dataObject (including new empty one)", async function(assert) {
     this.set("data", Ember.A(["first", "second"]));
     await render(hbs`
           {{#form-dynamic dataObject=data as |record|}}
@@ -29,8 +32,27 @@ module("Integration | Component | form-dynamic", function(hooks) {
           `);
     assert.equal(
       this.$("input").length,
-      this.get("data.length"),
+      2 + 1,
       "Count of yielded elements matches"
+    );
+  });
+
+  test("it yields content according to input data (including new empty one)", async function(assert) {
+    this.set("data", Ember.A(["first", "second"]));
+    await render(hbs`
+          {{#form-dynamic dataObject=data as |record|}}
+            <span>{{record}}</span><br />
+          {{/form-dynamic}}
+          `);
+    assert.equal(
+      this.$("span")[0].innerText,
+      this.get("data")[0],
+      "Content of the first element matches"
+    );
+    assert.equal(
+      this.$("span")[2].innerText,
+      "",
+      "Content of the last/empty element matches"
     );
   });
 });
