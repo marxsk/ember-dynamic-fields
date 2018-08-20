d077b8d15dcc56cbbb0790b490891b53e21fa9f3
Marek 'marx' Grác <mgrac@redhat.com>
2018-08-20 09:43:50 +0200

    [refactor]

--- addon/components/dynamic-fields.js
+++ addon/components/dynamic-fields.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
-import layout from '../templates/components/dynamic-fields';
+import Ember from "ember";
+import layout from "../templates/components/dynamic-fields";
 
 export default Ember.Component.extend({
   layout,
@@ -14,8 +14,8 @@ export default Ember.Component.extend({
    * Function that verifies if data in the record should be considered empty.
    * For a more complex element it makes sense to replace this function with user defined.
    */
-  isEmpty: (value) => {
-      return ((value === undefined) || (value.length === 0) || (value === ''));
+  isEmpty: value => {
+    return value === undefined || value.length === 0 || value === "";
   },
 
   _source: undefined,
@@ -27,59 +27,75 @@ export default Ember.Component.extend({
    * changing address of 'dataObject'.
    */
   init() {
-      this._super();
-      Ember.Logger.assert(this.get('dataObject') !== undefined, 'dynamic-fields: dataObject have to be defined');
+    this._super();
+    Ember.Logger.assert(
+      this.get("dataObject") !== undefined,
+      "dynamic-fields: dataObject have to be defined"
+    );
   },
 
   didReceiveAttrs() {
-    let x = this.get('dataObject');
-    if (this.get('dataObjectKey')) {
-      x = this.get(`dataObject.${this.get('dataObjectKey')}`);
+    let x = this.get("dataObject");
+    if (this.get("dataObjectKey")) {
+      x = this.get(`dataObject.${this.get("dataObjectKey")}`);
     }
 
     if (Ember.isArray(x)) {
-      Ember.Logger.debug('dynamic-fields: Input object was accepted');
-      this.set('_source', x);
+      Ember.Logger.debug("dynamic-fields: Input object was accepted");
+      this.set("_source", x);
     } else if (x !== undefined) {
-      Ember.Logger.assert('Input object have to be Ember.Array if you want to pass data in');
+      Ember.Logger.assert(
+        "Input object have to be Ember.Array if you want to pass data in"
+      );
     }
   },
 
   actions: {
     // target is not used at all but it is required because of ember-power-select
     // arg3 is either value or jQuery event object
-     update: function(target, targetIndex, arg3) {
-       Ember.Logger.assert(typeof targetIndex === "number", 'dynamic-fields: targetIndex parameter for update() should be number');
+    update: function(target, targetIndex, arg3) {
+      Ember.Logger.assert(
+        typeof targetIndex === "number",
+        "dynamic-fields: targetIndex parameter for update() should be number"
+      );
 
-       let value;
-       /*global jQuery*/
-       if ((arg3.__proto__ === jQuery.Event.prototype) || (arg3.__proto__ === Event.prototype)) {
-         value = arg3.target.value;
-       } else {
-         value = arg3
-       }
+      let value;
+      /*global jQuery*/
+      if (
+        arg3.__proto__ === jQuery.Event.prototype ||
+        arg3.__proto__ === Event.prototype
+      ) {
+        value = arg3.target.value;
+      } else {
+        value = arg3;
+      }
 
-       // note initialization
-      if (this.get('_source') === undefined) {
-        Ember.Logger.debug('dynamic-fields: Source was undefined, creating the array with empty item');
-        this.set('_source', Ember.A());
+      // note initialization
+      if (this.get("_source") === undefined) {
+        Ember.Logger.debug(
+          "dynamic-fields: Source was undefined, creating the array with empty item"
+        );
+        this.set("_source", Ember.A());
 
         // @todo-note: if dataObjectKey is undefined then use dataObject directly
-        if (this.get('dataObjectKey')) {
-          this.get('dataObject').set(this.get('dataObjectKey'), this.get('_source'));
+        if (this.get("dataObjectKey")) {
+          this.get("dataObject").set(
+            this.get("dataObjectKey"),
+            this.get("_source")
+          );
         } else {
-          this.set('dataObject', this.get('_source'));
+          this.set("dataObject", this.get("_source"));
         }
       }
 
-      if (targetIndex > (this.get('_source.length') - 1)) {
-        this.get('_source').pushObject(value);
+      if (targetIndex > this.get("_source.length") - 1) {
+        this.get("_source").pushObject(value);
       } else {
-        if (this.get('isEmpty')(value)) {
+        if (this.get("isEmpty")(value)) {
           Ember.Logger.debug(`dynamic-fields: Removing field ${targetIndex}`);
-          this.get('_source').removeAt(targetIndex);
+          this.get("_source").removeAt(targetIndex);
         } else {
-          this.get('_source').replace(targetIndex, 1, [value]);
+          this.get("_source").replace(targetIndex, 1, [value]);
         }
       }
     }
--- tests/integration/components/dynamic-fields-test.js
+++ tests/integration/components/dynamic-fields-test.js
@@ -1,28 +1,46 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import { fillIn } from 'ember-native-dom-helpers';
-import { skip } from 'qunit';
-import hbs from 'htmlbars-inline-precompile';
-import Ember from 'ember';
-import { typeInSearch, clickTrigger, selectChoose } from '../../helpers/ember-power-select';
+import { moduleForComponent, test } from "ember-qunit";
+import { fillIn } from "ember-native-dom-helpers";
+import { skip } from "qunit";
+import hbs from "htmlbars-inline-precompile";
+import Ember from "ember";
+import {
+  typeInSearch,
+  clickTrigger,
+  selectChoose
+} from "../../helpers/ember-power-select";
 
-moduleForComponent('dynamic-fields', 'Integration | Component | dynamic fields', {
-  integration: true
-});
+moduleForComponent(
+  "dynamic-fields",
+  "Integration | Component | dynamic fields",
+  {
+    integration: true
+  }
+);
 
-test('it yields content once when there are no data', function(assert) {
+test("*it yields content once when there are no data", function(assert) {
   this.render(hbs`{{dynamic-fields}}`);
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    ""
+  );
 
   this.render(hbs`
     {{#dynamic-fields}}
       template block text
     {{/dynamic-fields}}
   `);
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    "template block text"
+  );
 });
 
-test('it yields content according to number of records from dataObject', function(assert) {
-  this.set('data', Ember.A(['first', 'second']));
+test("*it yields content according to number of records from dataObject", function(assert) {
+  this.set("data", Ember.A(["first", "second"]));
   this.render(hbs`
     {{#dynamic-fields dataObject=data as |record index dynamicUpdate|}}
       <input
@@ -32,15 +50,23 @@ test('it yields content according to number of records from dataObject', functio
     {{/dynamic-fields}}
     `);
 
-  const input1 = this.$('input')[0];
+  const input1 = this.$("input")[0];
 
-  assert.equal(input1.value, 'first', 'The first input box contains right string');
-  assert.equal(3, this.$('input').length, 'There are two filled input boxes and one empty');
+  assert.equal(
+    input1.value,
+    "first",
+    "The first input box contains right string"
+  );
+  assert.equal(
+    3,
+    this.$("input").length,
+    "There are two filled input boxes and one empty"
+  );
 });
 
-test('it yields content according to number of records from dataObjectKey', function(assert) {
-  this.set('data', Ember.Object.create());
-  this.set('data.foo', Ember.A(['first', 'second']));
+test("it yields content according to number of records from dataObjectKey", function(assert) {
+  this.set("data", Ember.Object.create());
+  this.set("data.foo", Ember.A(["first", "second"]));
   this.render(hbs`
     {{#dynamic-fields dataObject=data dataObjectKey='foo' as |record index dynamicUpdate|}}
       <input
@@ -50,14 +76,22 @@ test('it yields content according to number of records from dataObjectKey', func
     {{/dynamic-fields}}
     `);
 
-  const input1 = this.$('input')[0];
+  const input1 = this.$("input")[0];
 
-  assert.equal(input1.value, 'first', 'The first input box contains right string');
-  assert.equal(3, this.$('input').length, 'There are two filled input boxes and one empty');
+  assert.equal(
+    input1.value,
+    "first",
+    "The first input box contains right string"
+  );
+  assert.equal(
+    3,
+    this.$("input").length,
+    "There are two filled input boxes and one empty"
+  );
 });
 
-test('it adds new line when last one is not empty', async function(assert) {
-  this.set('data', Ember.Object.create());
+test("it adds new line when last one is not empty", async function(assert) {
+  this.set("data", Ember.Object.create());
   this.render(hbs`
     {{#dynamic-fields dataObject=data as |record index dynamicUpdate|}}
       <input
@@ -66,25 +100,45 @@ test('it adds new line when last one is not empty', async function(assert) {
       />
     {{/dynamic-fields}}
     `);
-  assert.equal(1, this.$('input').length, 'There is one input box');
-  const input1 = this.$('input')[0];
+  assert.equal(1, this.$("input").length, "There is one input box");
+  const input1 = this.$("input")[0];
 
-  await fillIn(input1, 'first');
-  assert.equal(2, this.$('input').length, 'There are two input boxes because first one contains text');
+  await fillIn(input1, "first");
+  assert.equal(
+    2,
+    this.$("input").length,
+    "There are two input boxes because first one contains text"
+  );
 
-  const input2 = this.$('input')[1];
-  await fillIn(input1, 'new-first');
-  assert.equal(2, this.$('input').length, 'There are still two input boxes because only first one contains text');
+  const input2 = this.$("input")[1];
+  await fillIn(input1, "new-first");
+  assert.equal(
+    2,
+    this.$("input").length,
+    "There are still two input boxes because only first one contains text"
+  );
 
-  await fillIn(input2, 'second');
-  assert.equal(3, this.$('input').length, 'There are three input boxes because second one contains text');
+  await fillIn(input2, "second");
+  assert.equal(
+    3,
+    this.$("input").length,
+    "There are three input boxes because second one contains text"
+  );
 
-  assert.equal('new-first', this.get('data')[0], 'First element in the result is okay');
-  assert.equal('second', this.get('data')[1], 'Second element in the result is okay');
+  assert.equal(
+    "new-first",
+    this.get("data")[0],
+    "First element in the result is okay"
+  );
+  assert.equal(
+    "second",
+    this.get("data")[1],
+    "Second element in the result is okay"
+  );
 });
 
 test('it adds new line when last one is not empty and there is less than "max" lines', async function(assert) {
-  this.set('data', Ember.Object.create());
+  this.set("data", Ember.Object.create());
   this.render(hbs`
     {{#dynamic-fields dataObject=data max=2 as |record index dynamicUpdate|}}
       <input
@@ -93,25 +147,45 @@ test('it adds new line when last one is not empty and there is less than "max" l
       />
     {{/dynamic-fields}}
     `);
-  assert.equal(1, this.$('input').length, 'There is one input box');
-  const input1 = this.$('input')[0];
+  assert.equal(1, this.$("input").length, "There is one input box");
+  const input1 = this.$("input")[0];
 
-  await fillIn(input1, 'first');
-  assert.equal(2, this.$('input').length, 'There are two input boxes because first one contains text');
+  await fillIn(input1, "first");
+  assert.equal(
+    2,
+    this.$("input").length,
+    "There are two input boxes because first one contains text"
+  );
 
-  const input2 = this.$('input')[1];
-  await fillIn(input1, 'new-first');
-  assert.equal(2, this.$('input').length, 'There are still two input boxes because only first one contains text');
+  const input2 = this.$("input")[1];
+  await fillIn(input1, "new-first");
+  assert.equal(
+    2,
+    this.$("input").length,
+    "There are still two input boxes because only first one contains text"
+  );
 
-  await fillIn(input2, 'second');
-  assert.equal(2, this.$('input').length, 'There are still just two input boxes because of "max" limit');
+  await fillIn(input2, "second");
+  assert.equal(
+    2,
+    this.$("input").length,
+    'There are still just two input boxes because of "max" limit'
+  );
 
-  assert.equal('new-first', this.get('data')[0], 'First element in the result is okay');
-  assert.equal('second', this.get('data')[1], 'Second element in the result is okay');
+  assert.equal(
+    "new-first",
+    this.get("data")[0],
+    "First element in the result is okay"
+  );
+  assert.equal(
+    "second",
+    this.get("data")[1],
+    "Second element in the result is okay"
+  );
 });
 
-test('it adds new line when last one is not empty (with dataObjectKey)', async function(assert) {
-  this.set('data', Ember.Object.create());
+test("it adds new line when last one is not empty (with dataObjectKey)", async function(assert) {
+  this.set("data", Ember.Object.create());
 
   this.render(hbs`
     {{#dynamic-fields dataObject=data dataObjectKey='foo' as |record index dynamicUpdate|}}
@@ -123,25 +197,45 @@ test('it adds new line when last one is not empty (with dataObjectKey)', async f
       </span>
     {{/dynamic-fields}}
     `);
-  assert.equal(1, this.$('input').length, 'There is one input box');
-  const input1 = this.$('input')[0];
+  assert.equal(1, this.$("input").length, "There is one input box");
+  const input1 = this.$("input")[0];
 
-  await fillIn(input1, 'first');
-  assert.equal(2, this.$('input').length, 'There are two input boxes because first one contains text');
+  await fillIn(input1, "first");
+  assert.equal(
+    2,
+    this.$("input").length,
+    "There are two input boxes because first one contains text"
+  );
 
-  const input2 = this.$('input')[1];
-  await fillIn(input1, 'second');
-  assert.equal(2, this.$('input').length, 'There are still two input boxes because first one contains text');
+  const input2 = this.$("input")[1];
+  await fillIn(input1, "second");
+  assert.equal(
+    2,
+    this.$("input").length,
+    "There are still two input boxes because first one contains text"
+  );
 
-  await fillIn(input2, 'second');
-  assert.equal(3, this.$('input').length, 'There are three input boxes because second one contains text');
+  await fillIn(input2, "second");
+  assert.equal(
+    3,
+    this.$("input").length,
+    "There are three input boxes because second one contains text"
+  );
 
-  assert.equal('second', this.get('data.foo')[0], 'First element in the result is okay');
-  assert.equal('second', this.get('data.foo')[1], 'Second element in the result is okay');
+  assert.equal(
+    "second",
+    this.get("data.foo")[0],
+    "First element in the result is okay"
+  );
+  assert.equal(
+    "second",
+    this.get("data.foo")[1],
+    "Second element in the result is okay"
+  );
 });
 
-test('it removes line when it is empty and it is not last one', async function(assert) {
-  this.set('data', Ember.Object.create());
+test("it removes line when it is empty and it is not last one", async function(assert) {
+  this.set("data", Ember.Object.create());
 
   this.render(hbs`
     {{#dynamic-fields dataObject=data as |record index dynamicUpdate|}}
@@ -153,19 +247,27 @@ test('it removes line when it is empty and it is not last one', async function(a
       </span>
     {{/dynamic-fields}}
     `);
-  assert.equal(1, this.$('input').length, 'There is one input box');
-  const input1 = this.$('input')[0];
+  assert.equal(1, this.$("input").length, "There is one input box");
+  const input1 = this.$("input")[0];
 
-  await fillIn(input1, 'first');
-  assert.equal(2, this.$('input').length, 'There are two input boxes because first one contains text');
+  await fillIn(input1, "first");
+  assert.equal(
+    2,
+    this.$("input").length,
+    "There are two input boxes because first one contains text"
+  );
 
-  await fillIn(input1, '');
-  assert.equal(1, this.$('input').length, 'There is just one input box because first one was empty');
+  await fillIn(input1, "");
+  assert.equal(
+    1,
+    this.$("input").length,
+    "There is just one input box because first one was empty"
+  );
 });
 
-test('it works with ember-power-select', async function(assert) {
-  this.set('data', Ember.Object.create());
-  this.set('options', ['abc', 'abc2', 'def', 'xyz']);
+test("it works with ember-power-select", async function(assert) {
+  this.set("data", Ember.Object.create());
+  this.set("options", ["abc", "abc2", "def", "xyz"]);
 
   this.render(hbs`
     {{#dynamic-fields dataObject=data as |record index dynamicUpdate|}}
@@ -182,21 +284,41 @@ test('it works with ember-power-select', async function(assert) {
   `);
 
   await clickTrigger();
-  assert.equal(Ember.$('.ember-power-select-dropdown').length, 1, 'Dropdown is rendered');
-  assert.equal(Ember.$('.ember-power-select-option').length, this.get('options').length, 'Dropdown contains all items');
+  assert.equal(
+    Ember.$(".ember-power-select-dropdown").length,
+    1,
+    "Dropdown is rendered"
+  );
+  assert.equal(
+    Ember.$(".ember-power-select-option").length,
+    this.get("options").length,
+    "Dropdown contains all items"
+  );
 
-  await typeInSearch('ab');
+  await typeInSearch("ab");
   // only abc and abc2 options are visible
-  assert.equal(Ember.$('.ember-power-select-option').length, 2, 'Dropdown contains matching items after filtering');
-  await selectChoose('', 'abc');
+  assert.equal(
+    Ember.$(".ember-power-select-option").length,
+    2,
+    "Dropdown contains matching items after filtering"
+  );
+  await selectChoose("", "abc");
 
-  assert.equal(this.get('data.firstObject'), 'abc', 'Selected element is available in dataObject');
-  assert.equal(Ember.$('.ember-power-select-trigger').length, 2, 'Two ember-power-select are rendered');
+  assert.equal(
+    this.get("data.firstObject"),
+    "abc",
+    "Selected element is available in dataObject"
+  );
+  assert.equal(
+    Ember.$(".ember-power-select-trigger").length,
+    2,
+    "Two ember-power-select are rendered"
+  );
 });
 
-test('it works with ember-power-select-multiple', async function(assert) {
-  this.set('data', Ember.Object.create());
-  this.set('options', ['abc', 'abc2', 'def', 'xyz']);
+test("it works with ember-power-select-multiple", async function(assert) {
+  this.set("data", Ember.Object.create());
+  this.set("options", ["abc", "abc2", "def", "xyz"]);
 
   this.render(hbs`
     {{#dynamic-fields dataObject=data as |record index dynamicUpdate|}}
@@ -213,19 +335,39 @@ test('it works with ember-power-select-multiple', async function(assert) {
   `);
 
   await clickTrigger();
-  assert.equal(Ember.$('.ember-power-select-dropdown').length, 1, 'Dropdown is rendered');
-  assert.equal(Ember.$('.ember-power-select-option').length, this.get('options').length, 'Dropdown contains all items');
+  assert.equal(
+    Ember.$(".ember-power-select-dropdown").length,
+    1,
+    "Dropdown is rendered"
+  );
+  assert.equal(
+    Ember.$(".ember-power-select-option").length,
+    this.get("options").length,
+    "Dropdown contains all items"
+  );
 
-  await typeInSearch('ab');
+  await typeInSearch("ab");
   // only abc and abc2 options are visible and only visible options can be selectChoose-d
-  assert.equal(Ember.$('.ember-power-select-option').length, 2, 'Dropdown contains matching items after filtering');
-  await selectChoose('', 'abc');
-  await selectChoose('', 'xyz');
+  assert.equal(
+    Ember.$(".ember-power-select-option").length,
+    2,
+    "Dropdown contains matching items after filtering"
+  );
+  await selectChoose("", "abc");
+  await selectChoose("", "xyz");
 
-  assert.deepEqual(this.get('data.firstObject'), ['abc', 'xyz'], 'There are two selected items in the first field');
-  assert.equal(Ember.$('.ember-power-select-trigger').length, 2, 'Two ember-power-select-multiple are rendered');
+  assert.deepEqual(
+    this.get("data.firstObject"),
+    ["abc", "xyz"],
+    "There are two selected items in the first field"
+  );
+  assert.equal(
+    Ember.$(".ember-power-select-trigger").length,
+    2,
+    "Two ember-power-select-multiple are rendered"
+  );
 });
 
-skip('it works with ember-form-for', function(assert) {
+skip("it works with ember-form-for", function(assert) {
   assert.ok(false);
 });
